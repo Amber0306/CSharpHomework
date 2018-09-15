0637461c63de3db91040e74dccf5f1996e8b9470
Amber0306 <862268273@qq.com>
2018-09-15 23:40:41 +0800

    second homework

---
+++ homework2/program1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AnotherWay();
+
+            Console.WriteLine("请输入一个整数：");
+            string s = Console.ReadLine();
+            int number = Int32.Parse(s);
+            Console.WriteLine("该整数的素数因子有：");
+            for(int i = 2;i<number;i++)
+            {
+                //先求所有小于number的素数
+                bool prime = true;
+                for(int j=2;j<i;j++)
+                {
+                    if(i%j==0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                }
+                if(prime)
+                {
+                    if(0==number%i)//判断这些素数里哪些是number的因子
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+        }
+        static void AnotherWay()
+        {
+           //输出包括次方
+            Console.WriteLine("请输入一个整数：");
+            string s =Console.ReadLine();
+            int number = Int32.Parse(s);
+            int temp = number;
+            Console.WriteLine("该整数的素数因子有：");
+            int i = 2;
+            while (temp >= i)//temp小于除数（待选因子）时结束循环
+            {
+                if (temp % i == 0)
+                {
+                    Console.WriteLine(i);
+                    temp = temp/ i;
+                }
+                else
+                    i++;
+            }
+            if (temp != 1)
+                Console.WriteLine(temp); 
+        }
+    }
+}
---
+++ homework2/program2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //获取数据
+            Console.WriteLine("请输入数组元素个数:");
+            string s = Console.ReadLine();
+            int length = Int32.Parse(s);
+            int [] array = new int[length];
+            for(int i = 0;i<length;i++)
+            {
+                Console.WriteLine("请输入第 "+(i+1)+" 个元素");
+                string str = Console.ReadLine();
+                array[i] = Int32.Parse(str);
+            }
+            //从小到大排序
+            for (int j = 0; j < length; j++)
+            {
+                for (int k = 0; k < length - j - 1; k++)
+                {
+                    if (array[k] > array[k + 1])
+                    {
+                        int temp = array[k];
+                        array[k] = array[k + 1];
+                        array[k + 1] = temp;
+                    }
+                }
+            }
+            //输出
+            Console.WriteLine("数组的最大值是：");
+            Console.WriteLine(array[length-1]);
+            Console.WriteLine("数组的最小值是：");
+            Console.WriteLine(array[0]);
+            double sum = 0;
+            for(int i=0;i<length;i++)
+            {
+                sum += array[i];
+            }
+            double average = sum / length;
+            Console.WriteLine("数组的平均值是：");
+            Console.WriteLine(average);
+            Console.WriteLine("数组所有元素的和是：");
+            Console.WriteLine(sum);
+        }
+    }
+}
